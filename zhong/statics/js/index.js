49b87a89a0586fcb46520ad1514410241867b6f9
zhaoshaoyang <1187391346@qq.com>
2019-04-16 23:59:00 +0800

    首页适配

--- zhong/statics/js/index.js
+++ zhong/statics/js/index.js
@@ -1,17 +1,18 @@
 var common = {
-    init: function() {
+    init: function () {
         lazyload();
         this.animationFun('fadeInUp');
+        this.scrollHeader();
         //返回顶部
-        if($('.js-backtop')){
+        if ($('.js-backtop')) {
             $('.js-backtop').click(function () {
                 $("html, body").animate({
                     'scrollTop': 0
-                },600);
+                }, 600);
                 return false;
             });
         }
-        if($('.js-close')){
+        if ($('.js-close')) {
             $('.js-close').click(function () {
                 $(".search-form").hide();
             })
@@ -20,7 +21,7 @@ var common = {
         /*移动端头部收展*/
         $('.mb-aside-nav .js-list-name').click(function () {
             $(this).siblings('.sub-list').slideToggle();
-            if(!$(this).parent().hasClass('active')){
+            if (!$(this).parent().hasClass('active')) {
                 $(this).parent().addClass('active');
             }
             $(this).parent().siblings('.list-item').removeClass('active');
@@ -28,30 +29,57 @@ var common = {
         });
         $('.js-nav-icon').click(function () {
             $('.mb-aside-nav').show();
-            $('.mb-aside-nav .nav-list').stop().animate({'right':0});
+            $('.mb-aside-nav .nav-list').stop().animate({'right': 0});
         });
         $('.js-close-icon').click(function () {
-            $('.mb-aside-nav .nav-list').stop().animate({'right':'-77%'});
+            $('.mb-aside-nav .nav-list').stop().animate({'right': '-77%'});
             $('.mb-aside-nav').hide();
         });
         $('.mb-aside-nav-left').click(function () {
-            $('.mb-aside-nav .nav-list').stop().animate({'right':'-77%'});
+            $('.mb-aside-nav .nav-list').stop().animate({'right': '-77%'});
             $('.mb-aside-nav').hide();
         })
     },
     animationFun: function (className) {
-    var $offsetTop = $(window).height();
-    var $elment = ".js-" + className;
-    var waypoints1 = $($elment).waypoint({
-      handler: function handler(direction) {
-        if (direction === "down") {
-          $(this.element).addClass(className); // }else {
-          //     $(this.element).removeClass("animated fadeInUp")
+        var $offsetTop = $(window).height();
+        var $elment = ".js-" + className;
+        var waypoints1 = $($elment).waypoint({
+            handler: function handler(direction) {
+                if (direction === "down") {
+                    $(this.element).addClass(className); // }else {
+                    //     $(this.element).removeClass("animated fadeInUp")
+                }
+            },
+            offset: $offsetTop
+        });
+    },
+    scrollHeader:  function () {
+        var beforeScrollTop = $(window).scrollTop();
+        var $header = $(".zs-header");
+        if(beforeScrollTop>0) {
+            $header.removeClass('opacity');
+        }else {
+            $header.addClass('opacity');
         }
-      },
-      offset: $offsetTop
-    });
-  }
+        $(window).scroll(function () {
+            var afterScrollTop = $(window).scrollTop();
+            if(!$header.hasClass("none")) {
+                if(afterScrollTop-beforeScrollTop>0) {
+                    $header.addClass('active');
+                }else if(afterScrollTop-beforeScrollTop==0) {
+                    return false
+                }else{
+                    $header.removeClass('active');
+                }
+            }
+            beforeScrollTop = $(window).scrollTop();
+            if(beforeScrollTop>0) {
+                $header.removeClass('opacity');
+            }else {
+                $header.addClass('opacity');
+            }
+        })
+    }
 }
 $(function () {
     common.init();
